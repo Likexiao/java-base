c2ce6a5270d1c91f548f642a33005678e861cc99
likexiao <1932977254@qq.com>
2022-02-22 10:05:53 +0800

    test

--- src/main/java/com/java/base/thread/ReentrantLockTest.java
+++ src/main/java/com/java/base/thread/ReentrantLockTest.java
@@ -1,4 +1,34 @@
 package com.java.base.thread;
 
-public class ReentrantLockTest {
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest implements Runnable{
+    private static final Lock lock = new ReentrantLock();
+    private static int sign = 1;
+
+    @Override
+    public void run() {
+        lock.lock();
+        System.out.println(Thread.currentThread().getName()+"拿了锁");
+        try {
+            System.out.println("sign : "+sign);
+
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            sign = 20;
+            System.out.println(Thread.currentThread().getName()+"释放了锁");
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ReentrantLockTest());
+        Thread thread2 = new Thread(new ReentrantLockTest());
+        thread1.start();
+        thread2.start();
+
+    }
 }
